514ad63418dabd06677ce0eebe23650f744a7993
UmudovRavan <umudova1985@gmail.com>
2024-12-07 14:01:58 +0400

    Created new file

---
+++ WeaponTask/WeaponTask/Program.cs
@@ -0,0 +1,159 @@
+﻿using WeaponTask;
+
+namespace WeaponTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int magazineCapacity = 0;
+            Console.Write("Daragin tutumunu daxil edin(30 ,40 ,50 ,60 ): ");
+            if (int.TryParse(Console.ReadLine(), out magazineCapacity) &&
+                (magazineCapacity == 30 || magazineCapacity == 40 || magazineCapacity == 60 || magazineCapacity == 90))
+            {
+                Console.WriteLine("Daragin gulle tutumu: " + magazineCapacity);
+            }
+            else
+            {
+                throw new Exception("Yanlis deyer daxil edildi.Daragin tutumu yalniz 30,40,50,60 ola biler");
+            }
+
+
+
+            Console.Write("Daraqdaki gulle sayini daxil edin: ");
+            int bulletsInMagazine = 0;
+            if (int.TryParse(Console.ReadLine(), out bulletsInMagazine) &&
+                (bulletsInMagazine <= magazineCapacity))
+            {
+                Console.WriteLine("Daraqdaki gulle sayi: " + bulletsInMagazine);
+            }
+            else
+            {
+                throw new Exception("Daraqdaki gulle sayi tutumundan cox ola bilmez.");
+            }
+
+
+
+            Console.Write("Select the firing mode (1 - Single, 2 - Burst, 3 - Automatic): ");
+            string? input = Console.ReadLine();
+
+            Weapon.FireMode mode = (Weapon.FireMode)int.Parse(input);
+            if (input == "1" || input == "2" || input == "3")
+            {
+                Console.WriteLine("Secilmis atis modu: " + mode);
+
+            }
+            else
+            {
+                throw new Exception("Yanlis deyer daxil edildi.Atis modlari yalniz 1,2 ve 3 ola biler");
+            }
+
+            Weapon weapon = new Weapon(magazineCapacity, bulletsInMagazine, mode);
+
+
+
+
+
+            Console.WriteLine("\n0 - İnformasiya almaq üçün");
+            Console.WriteLine("1 - Shoot metodu üçün");
+            Console.WriteLine("2 - Fire metodu üçün");
+            Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+            Console.WriteLine("4 - Reload metodu üçün");
+            Console.WriteLine("5 - ChangeFireMode metodu üçün");
+            Console.WriteLine("7 - Tutumu deyissin");
+            Console.WriteLine("8 - Gulle sayi deyissin");
+            Console.WriteLine("9 - Proqramdan çıxmaq üçün");
+
+
+            while (true)
+            {
+
+                Console.Write("Seçiminizi edin: ");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine($"Darağın tutumu: {weapon.MagazineCapasity}");
+                        Console.WriteLine($"Daraqdakı güllə sayı: {weapon.BulletsInMagazine}");
+                        Console.WriteLine($"Atış modu: {weapon.Mode}");
+                        break;
+                    case 1:
+                        weapon.Shot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        Console.WriteLine("Tam doldurmaq üçün lazım olan güllə sayı: " + weapon.GetRemainBulletCount());
+                        break;
+
+                    case 4:
+                        weapon.Reload();
+                        break;
+
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Proqram dayandırılır...");
+                        return;
+
+                    case 7:
+                        Console.WriteLine("8 - Tutumu dəyişmək");
+                        Console.WriteLine("9 - Güllə sayını dəyişmək");
+                        break;
+
+                    case 8:
+                        Console.WriteLine("Yeni tutum daxil edin:");
+                        int newCapacity = int.Parse(Console.ReadLine());
+              
+
+                        if(int.TryParse(Console.ReadLine(), out newCapacity) && 
+                            (newCapacity == 30 || newCapacity == 40 || newCapacity == 60 || newCapacity == 90))
+                        {
+                            Console.WriteLine("Daragin yeni tutumu: " + newCapacity);
+                        }
+                        else
+                        {
+                            throw new Exception("Yanlis deyer daxil edildi.Daragin yeni tutumu yalniz 30,40,50,60 ola biler");
+                        }
+
+                        weapon.ChangeMagazineCapacity(newCapacity);
+                        break;
+                    case 9:
+                        Console.Write("Yeni gulle sayini daxil edin: ");
+                        int newBulletCount = int.Parse(Console.ReadLine());
+
+                
+
+                        if (int.TryParse(Console.ReadLine(), out newBulletCount) && 
+                            (newBulletCount <= magazineCapacity))
+                        {
+                            Console.WriteLine("Daragdaki yeni gulle sayi: " + newBulletCount);
+                        }
+                        else
+                        {
+                            throw new Exception("Daraqdaki yeni gulle sayi tutumundan cox ola bilmez.");
+                        }
+
+
+                        weapon.ChangedBulletInMagazine(newBulletCount);
+                        break;
+
+                    default:
+                        Console.WriteLine("Yanlış seçim! Yenidən cəhd edin.");
+                        break;
+
+
+
+
+
+                }
+            }
+        }
+    }
+}
---
+++ WeaponTask/WeaponTask/Weapon.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeaponTask;
+
+namespace WeaponTask
+{
+    public class Weapon
+    {
+        public enum FireMode
+        {
+            Single =1,
+            Burst,
+            Automatic
+        }
+
+        public int MagazineCapasity { get; private set; }
+        public int BulletsInMagazine { get; private set; }
+
+        public FireMode Mode { get; private set; }
+
+        public Weapon(int magazineCapasity, int bulletInMagazine, FireMode mode)
+        {
+            if (magazineCapasity < 0)
+            {
+                throw new Exception("Daragin tutumu 0-dan boyuk olmalidir.");
+            }
+
+
+            if (bulletInMagazine > magazineCapasity || bulletInMagazine < 0)
+            {
+                throw new Exception("Daraqdaki gulle sayi duzgun teyin edilmeyib.");
+            }
+
+            MagazineCapasity = magazineCapasity;
+            BulletsInMagazine = bulletInMagazine;
+            Mode = mode;
+        }
+
+        public void Shot()
+        {
+            if (BulletsInMagazine > 0)
+            {
+                BulletsInMagazine--;
+                Console.WriteLine("Bir gulle atildi qalan gulle sayi: " + BulletsInMagazine);
+            }
+            
+            else
+            {
+                Console.WriteLine("Daraq bosdur yeniden doldurun.");
+            }
+        }
+
+        public void Fire()
+        {
+            if (Mode == FireMode.Automatic)
+            {
+                if (BulletsInMagazine > 0)
+                {
+                    Console.WriteLine("Butun gulleler atilir.");
+                    BulletsInMagazine = 0;
+                    Console.WriteLine("Qalan gulle sayi: " + BulletsInMagazine);
+                }
+                else
+                {
+                    Console.WriteLine("Daraq bosdur yeniden doldurun.");
+                }
+            }
+            else if (Mode == FireMode.Burst)
+            {
+                BurstShoot();
+            }
+            else
+            {
+                Console.WriteLine("Tekli modda Fire() istifade edile bilmez.Shoot() cagirin.");
+            }
+        }
+
+        private void BurstShoot()
+        {
+            if (BulletsInMagazine >= 3)
+            {
+                BulletsInMagazine -= 3;
+                Console.WriteLine("Uc gulle atildi.Qalan gulle sayi:" + BulletsInMagazine);
+            }
+            else if (BulletsInMagazine > 0)
+            {
+                Console.WriteLine($"Yalniz {BulletsInMagazine} gulle atildi.Daraq bosaldi.");
+                BulletsInMagazine = 0;
+            }
+            else
+            {
+                Console.WriteLine("Daraq bosdur yeniden doldurun.");
+            }
+        }
+        public int GetRemainBulletCount()
+        {
+            return MagazineCapasity - BulletsInMagazine;
+        }
+
+        public void Reload()
+        {
+            int bulletsNeed = GetRemainBulletCount();
+            BulletsInMagazine += bulletsNeed;
+
+            Console.WriteLine("Daraq dolduruldu.Movcud gulle sayi: " + BulletsInMagazine);
+
+        }
+
+        public void ChangeFireMode()
+        {
+            switch(Mode)
+            {
+                case FireMode.Single:
+                    Mode = FireMode.Burst;
+                    break;
+                case FireMode.Burst:
+                    Mode = FireMode.Automatic;
+                    break;
+                case FireMode.Automatic:
+                    Mode = FireMode.Single;
+                    break;
+            }
+            Console.WriteLine($"Atis modu deyisdi: {Mode}");
+            
+        }
+
+        public void ChangeMagazineCapacity(int newCapacity)
+        {
+            if (MagazineCapasity > newCapacity)
+                throw new Exception("Yeni tutum movcud gulle sayindan az ola bilmez.");
+            MagazineCapasity = newCapacity;
+            Console.WriteLine("Daragin gulle tutumu yenilendi: " + MagazineCapasity);
+        }
+
+        public void ChangedBulletInMagazine(int bullet)
+        {
+            if (bullet < 0 || bullet > MagazineCapasity)
+                throw new Exception("Gulle sayi duzgun teyin edilmiyib.");
+            BulletsInMagazine = bullet;
+            Console.WriteLine("Daraqdaki gulle sayi yenilendi: " + BulletsInMagazine);
+        }
+
+
+    }
+}
+
